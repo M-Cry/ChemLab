081f33d7d2f033c10559be09823ddd7653ffb438
MCry <42918650+M-Cry@users.noreply.github.com>
2023-06-04 22:19:33 +0400

    added washing hand

---
+++ Assets/Scripts/StepsManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepsManager : MonoBehaviour
+{
+
+    public GameObject[] Steps;
+    private int currentStepIndex;
+
+    [SerializeField]
+    private float waitTime = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Steps.Length; i++)
+        {
+            if (i == currentStepIndex)
+            {
+                Steps[i].SetActive(true);
+            }
+            else
+            {
+                Steps[i].SetActive(false);
+            }
+        }
+
+        if (currentStepIndex == 0)
+        {
+
+        }
+        else if (currentStepIndex == 1)
+        {
+            if (waitTime <= 0)
+            {
+
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WashHand.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashHand : MonoBehaviour, IInteractable
+{
+    [SerializeField] string interactText;
+
+    [SerializeField] ParticleSystem runningWater;
+
+    [SerializeField] AudioSource openSound;
+
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+    public void Interact(Transform interactorTransform)
+    {
+        runningWater.Play();
+        openSound.Play();
+    }
+
+
+}
